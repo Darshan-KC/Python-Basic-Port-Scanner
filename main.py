04fc14a50ccf7d3d8b108d78354deee699274dd4
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-01-18 22:46:41 +0545

    main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Entry point to run the scanner
+
+def main():
+    target_host = input("Enter target host (e.g., 127.0.0.1): ")
+    start_port = int(input("Enter start port: "))
+    end_port = int(input("Enter end port: "))
+    
+if __name__ == "__main__":
+    main()
